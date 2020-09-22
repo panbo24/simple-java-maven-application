20629b47f1066ee799091855569d406b0309969a
panbo24 <296246015@qq.com>
2020-09-22 18:56:35 +0800

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,12 +7,9 @@ public class App
 {
 
     private final String message = "Hello World!";
-<<<<<<< HEAD
     private final String message2 = "Hello World2";
     private final String message3 = "Hello World3";
-=======
-
->>>>>>> parent of 97e9a6a... commitTest2
+    
     public App() {}
 
     public static void main(String[] args) {
