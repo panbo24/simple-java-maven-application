41f9c8f6215ad0d8035055316b2590fd93e3c0c4
panbo24 <296246015@qq.com>
2020-09-24 19:31:07 +0800

    revert e10c076a9e2eaafb19d37208bef40f1fa781e96e

---
+++ revert.sh
@@ -0,0 +1,14 @@
+# 循环遍历commit版本号文本的每一行
+for line in `cat commitNos`
+do
+ echo $line
+# 把版本号赋值给变量
+ commitNo=$line
+# 撤销当前版本号的commit操作
+ git revert $commitNo
+# 撤销后提交到本地仓库
+ git add .
+ git commit -m"revert ${commitNo}"
+# 推到远程github主分支上
+ git push origin master
+done
